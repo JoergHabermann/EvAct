c7fe5e43d863a0467cccd5ebb42f22581e14b716
Jörg Habermann <joerg1985@gmx.net>
2025-01-31 20:36:42 +0100

    fetched data

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,12 +1,85 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatSliderModule,
+    MatDatepickerModule,
+    FormsModule, ReactiveFormsModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './mainpage.component.html',
-  styleUrl: './mainpage.component.scss'
+  styleUrl: './mainpage.component.scss',
 })
 export class MainpageComponent {
+  searchType : string = '';
 
+  dataForm = new FormGroup({
+    rangeValue: new FormControl(0),
+    start: new FormControl(),
+    end: new FormControl(),
+    selectControl: new FormControl('school')  
+  });
+
+  locationData : any;
+  
+  constructor(){}  
+
+  async fetchData(range : number, type : string) {
+    
+    const query = `
+      [out:json];(
+        node["leisure"="fitness_centre"](around:20000,52.0499998, 10.3666652);        
+      );
+      out body qt;
+      out tags;
+    `;
+    const URL = await fetch(`https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`);
+    const URLtoJSON = await URL.json(); 
+    this.locationData = URLtoJSON.elements;
+
+  }
+
+  logDates() {
+    const startDate = this.dataForm.get('start')?.value;
+    const endDate = this.dataForm.get('end')?.value;
+    console.log(startDate);
+    console.log(endDate);
+  }
+
+  async logData() {
+    const range : number = this.dataForm.get('rangeValue')!.value!;
+    const type : string = this.dataForm.get('selectControl')!.value!;
+    this.logDates();
+    console.log(range);
+    console.log(type);    
+    await this.fetchData(range,type);
+    console.log(this.locationData);
+    
+  }
+
+  
+
+
+  formatLabel(value: number): string {
+    return value + 'km';
+  }
 }
